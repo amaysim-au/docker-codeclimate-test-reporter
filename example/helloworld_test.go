10e587f757b6a9b60915a823836669ed6ba4acc6
Frederic Lemay <frederic.lemay@amaysim.com.au>
2018-05-01 17:52:31 +1000

    revert test coverage changes

--- example/helloworld_test.go
+++ example/helloworld_test.go
@@ -1,12 +1,12 @@
 package example_test
 
 import (
-	// "github.com/amaysim-au/docker-codeclimate-test-reporter/example"
+	"github.com/amaysim-au/docker-codeclimate-test-reporter/example"
 	"testing"
 )
 
 func TestHelloWorld(t *testing.T) {
-	// if example.HelloWorld() != "Hello World" {
-	// 	t.Fail()
-	// }
+	if example.HelloWorld() != "Hello World" {
+		t.Fail()
+	}
 }
